d508833db50c59fb37ea63c806d996285a1c3268
Putnam14 <putnam@Bridgers-MacBook-Pro.local>
2018-11-03 21:52:51 -0600

    ThemeProvider support

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Header from './header'
 import './layout.css'
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '960px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 0 1.0875rem 1.45rem;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -18,28 +40,21 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          // Primary layout styling
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0 1.0875rem 1.45rem',
-          }}
-        >
-          {children}
-        </div>
-      </>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <Inner>{children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     )}
   />
 )
