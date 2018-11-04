839a939636e77a7adbf291f8aba1e34d917a5f6e
Putnam14 <putnam@Bridgers-MacBook-Pro.local>
2018-11-03 19:25:36 -0600

    Bring image query to the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
-import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Image imgPath="gatsby-astronaut.png" imgMax="300" />
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = props => {
+  const { data } = props
+  return (
+    <Layout>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+        <Img
+          title="Test image"
+          alt="This is a test image"
+          fluid={data.placeholderImage.childImageSharp.fluid}
+        />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query firstImage {
+    placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+// TODO Props validation
